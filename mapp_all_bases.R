52fb9cf9bf01709925ece8b6ec8fe29814abe8be
GERALDSSENOGA <126305882+GERALDSSENOGA@users.noreply.github.com>
2023-10-02 14:25:32 +0800

    Removed credentials in this files

--- mapp_all_bases.R
+++ mapp_all_bases.R
@@ -6,14 +6,12 @@ library(plyr)
 library(data.table)
 library(plyr)
 library(dplyr)
-library('DomoR')
 
-DomoR::init('caremin', 'eb7b72130d8b6283fe1fb6d447f769ad0a91f3cb1d44ca89')
-map_all <- DomoR::fetch('2e6c60c6-e758-407b-83a3-da70b8c97248')
+map_all <- DomoR::fetch('')
 sum(map_all$tf_graduated_participants)
 
 # Coomunity overview data
-commD <- DomoR::fetch('3d637c89-a96d-49eb-875e-187f62c19522')
+commD <- DomoR::fetch('')
 commDt <- commD %>% filter(base_name != 'Guatemala')
 sum(commDt$total_graduates)
 a <- commDt[is.na(commDt$pastor_id),] #%>% select(base_name,branch_name,batch_name,pastor_id,district_id,total_graduates)
